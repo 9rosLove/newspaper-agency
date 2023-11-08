b4c1a94736370c711099980adcf6ea4fa90ddce4
9rosLove <yarik2300@gmail.com>
2023-11-08 21:11:58 +0200

    update settings.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -141,7 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
